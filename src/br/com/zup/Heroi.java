4d75fb584431a2e3c6c57c31e55670cadee51643
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-09-23 11:57:47 -0300

    Modelando classe Herói

---
+++ src/br/com/zup/Heroi.java
@@ -0,0 +1,53 @@
+package br.com.zup;
+
+public class Heroi {
+
+    //atributos
+    private String nome;
+    private double vida;
+
+
+    //construtor
+    public Heroi(){
+
+    }
+
+    public Heroi(String nome, double vida){
+        this.nome = nome;
+        this.vida = vida;
+    }
+
+
+    //getters e setters
+    public String getNome() {
+        return nome;
+    }
+
+    public double getVida() {
+        return vida;
+    }
+
+    public void setVida(double vida) {
+        this.vida = vida;
+    }
+
+
+    //Método para trocar de nome
+    public void trocarNome(String nome){
+        this.nome = nome;
+    }
+
+
+    //Método para receber Dano
+    public  void receberDano(double dano){
+        double recebeDano = vida - dano;
+        vida = recebeDano;
+    }
+
+
+    //Método exibirVida
+    public double exibirVida(){
+        return vida;
+    }
+
+}
